f804d18cb1f051588cc0ba50593d9d996bbe292d
Gisela Alonso Martinez <108431284+galonsomartinez@users.noreply.github.com>
2022-10-26 10:37:16 -0400

    Update FeaturesSection.tsx

--- src/components/sections/FeaturesSection.tsx
+++ src/components/sections/FeaturesSection.tsx
@@ -18,7 +18,7 @@ const FeaturesSection = () => {
   return (
     <div className="relative bg-gray-100 py-16 sm:py-24 lg:py-32">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-        <h2 className="text-4xl font-semibold tracking-wider text-red-900 uppercase">
+        <h2 className="text-2xl font-semibold tracking-wider text-blue-600 uppercase">
           Configurable plans
         </h2>
         <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
